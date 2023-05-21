8020de2729ec5594006859e140aab79a25bc6c9c
David Arias <davidariascalderon04@gmail.com>
2023-05-21 18:11:43 -0500

    primera prueba para escribir en archivos de texto

--- prueba_agregacion.cpp
+++ prueba_agregacion.cpp
@@ -9,21 +9,39 @@ string leer(string bd);
 int main(){
     string bd = "candidatos";
     escribir(bd);
+    cout << leer(bd);
     system("pause");
     return 0;
 }
 
 
 void escribir(string bd){
-    ofstream ciudades;
+    ofstream archivo;
     bd.c_str();
     std::string s;
     s.append("C://Users//David//Desktop//Ciencias I//final//BD//").append(bd).append(".txt");
-    ciudades.open(s,ios::out);
-    if(ciudades.fail()){
+    archivo.open(s,ios::app);
+    if(archivo.fail()){
         cout<<"Error al crear el archivo";
     }
-    ciudades<<"Hola como estas?, Bien";
-    ciudades.close();
+    archivo<<"Hola como estas?, Bien";
+    archivo.close();
+}
+
+string leer(string bd){
+    ifstream archivo;
+    string texto;
+    bd.c_str();
+    std::string s;
+    s.append("C://Users//David//Desktop//Ciencias I//final//BD//").append(bd).append(".txt");
+    archivo.open(s,ios::in);
+    if (archivo.fail()){
+        cout<<"Error al abrir el archivo";
+    }
+    while(!archivo.eof()){
+        getline(archivo,texto);
+    }
+    archivo.close();
+    return texto;
 }
 
