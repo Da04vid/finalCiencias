68f7b4fb98badb4296a68de7d87c340f6aeb8f9f
David Arias <davidariascalderon04@gmail.com>
2023-05-21 19:50:11 -0500

    .h para guardar registros y menu

--- prueba_agregacion.cpp
+++ prueba_agregacion.cpp
@@ -7,7 +7,7 @@ void escribir(string bd);
 string leer(string bd);
 
 int main(){
-    string bd = "candidatos";
+    string bd = "ciudades";
     escribir(bd);
     cout << leer(bd);
     system("pause");
@@ -17,6 +17,9 @@ int main(){
 
 void escribir(string bd){
     ofstream archivo;
+    string nombre;
+    char respuesta;
+    int poblacion;
     bd.c_str();
     std::string s;
     s.append("C://Users//David//Desktop//Ciencias I//final//BD//").append(bd).append(".txt");
@@ -24,7 +27,18 @@ void escribir(string bd){
     if(archivo.fail()){
         cout<<"Error al crear el archivo";
     }
-    archivo<<"Hola como estas?, Bien";
+    do {
+        cout << "Ingrese el nombre de la ciudad: ";
+        cin >> nombre;
+
+        cout << "Ingrese la poblacion de la ciudad: ";
+        cin >> poblacion;
+
+        archivo << nombre << "," << poblacion << endl;
+
+        cout << "¿Desea agregar otra ciudad? (s/n): ";
+        cin >> respuesta;
+    } while (respuesta == 's' || respuesta == 'S');
     archivo.close();
 }
 
